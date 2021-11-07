cca4fc72f5422c1eb33314b69c6b37c62a595b81
Garpon4ikk <90471996+Garpon4ikk@users.noreply.github.com>
2021-11-07 20:30:03 +0300

    src test

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -1,48 +1,159 @@
 "use strict"
 
-let type = prompt('Какой тип сайта Вы бы себе хотели? (0 - пока не определился, 1 - корпоративный, 2 - личный, 3 - для приколов)', 0);
-let design = prompt('Какой дизайн сайты Вы бы себе хотели? (0 - пока не определился, 1 - строгий, 2 - ничё такой, 3 - прикольный)', 0);
-let adaptation = prompt('Какую адаптивность Вы бы хотели своему сайту? (0 - пока не определился, 1 - пека, 2 - телефония, 3 - два реальных аппарата)', 0);
+/*var delayInMilliseconds = 30000;
 
-let calc = 0;
+setTimeout(function() {
+  	if (confirm("Нужна помощь с выбором? Напищите мне!")) {
+		window.location.href = 'https://www.google.com/' };
+	  
+}, delayInMilliseconds);
+
+setTimeout(function(){
+     clearInterval(delayInMilliseconds);
+},29000);*/
+
+$(document).ready(function() {
+$(window).scroll(()=>{
+	let  scrollDistance = $(window).scrollTop();
+
+	$(".logo1").each((i, el)=>{
+
+		if ($(el).offset().top - $("nav").outerHeight() <= scrollDistance){
+			$("nav a").each((i, el)=>{
+				if ($(el).hasClass("active")){
+					$(el).removeClass("active");
+				}
+			});
+			$('nav li:eq('+ i +')').find('a').addClass('active');
+		}
+	});
+});
+});
+
+$('a[href^="#"]').click(function(){
+	let valHref = $(this).attr("href");
+	$('html, body').animate ({scrollTop: $(valHref).offset().top - 70 + "px"});
+}); 
+
+
+    var type = localStorage.getItem('message');
+    document.getElementById('message').innerHTML = x;
+
+var type = document.getElementById('type').value
+var design = document.getElementById('design').value
+var adaptation = document.getElementById('adaptation').value
+setInterval(function() {
+/*РАСЧЁТ МАНЕЙ*/
+var calc = 0;
+var calcT = 0;
 /*расчёт типа*/
-
 if (type == 1){
-	calc = calc + 1000;
-}
-
+	calc = calc + 1000;}
 if (type == 2){
-	calc = calc + 2000;
-}
-
+	calc = calc + 2000;}
 if (type == 3){
-	calc = calc + 3000;
-}
-
+	calc = calc + 3000;}
 /*расчёт дезигна*/
 if (design ==1) {
-	calc = calc + 1000;
-} 
-
+	calc = calc + 1000;} 
 if (design ==2) {
-	calc = calc + 2000;
-} 
-
+	calc = calc + 2000;} 
 if (design ==3) {
-	calc = calc + 3000;
-} 
-
+	calc = calc + 3000;} 
 /*расчёт адаптации*/
 if (adaptation ==1) {
-	calc = calc + 1000;;
-} 
-
+	calc = calc + 1000;;} 
 if (adaptation ==2) {
-	calc = calc + 2000;
-} 
-
+	calc = calc + 2000;} 
 if (adaptation ==3) {
-	calc = calc + 3000;
-}
+	calc = calc + 3000;}
 
-alert ('Стоимость сайта будет состовлять ' +calc+ ' дублонов')
+/*РАСЧЁТ ТАЙМОВ*/
+var calcT = 0;
+/*расчёт типа*/
+if (type == 1){
+	calcT = calcT + 3;}
+if (type == 2){
+	calcT = calcT + 2;}
+if (type == 3){
+	calcT = calcT + 1}
+/*расчёт дезигна*/
+if (design ==1) {
+	calcT = calcT + 3;} 
+if (design ==2) {
+	calcT = calcT + 2;} 
+if (design ==3) {
+	calcT = calcT + 1;} 
+/*расчёт адаптации*/
+if (adaptation ==1) {
+	calcT = calcT + 1;} 
+if (adaptation ==2) {
+	calcT = calcT + 2;} 
+if (adaptation ==3) {
+	calcT = calcT + 3;}
+document.getElementById("calcMoney").innerHTML = calc;
+document.getElementById("calcTime").innerHTML = calcT;
+}, 1);
+
+
+/*анимка лого*/
+$(document).ready (function (){
+let optionsL = {thereshold: [0.5]};
+let observerL = new IntersectionObserver (onEntry, optionsL);
+let elementsL = $('.logo-animation');
+elementsL.each ((i, el)=>{
+	observerL.observe(el);
+});
+function onEntry (entry){
+	entry.forEach (change =>{
+		if (change.isIntersecting){
+			change.target.classList.add('ShowLogo');
+		}
+	})
+}
+})
+
+
+
+
+$(document).ready (function () {
+  var logoAnim = document.querySelector('.logo-animation');
+
+  var observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (typeof getCurrentAnimationPreference === 'function' && !getCurrentAnimationPreference()) {
+        return;
+      }
+
+      if (entry.isIntersecting) {
+        entry.target.classList.add('ShowLogo');
+      }
+    });
+  });
+
+  observer.observe(logoAnim);
+});
+
+
+
+
+
+/*анимка статы*/
+$(document).ready (function () {
+  var stataAnim = document.querySelector('.stataAnim');
+
+  var observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (typeof getCurrentAnimationPreference === 'function' && !getCurrentAnimationPreference()) {
+        return;
+      }
+
+      if (entry.isIntersecting) {
+        entry.target.classList.add('stataAnim-animation');
+      }
+    });
+  });
+
+  observer.observe(stataAnim);
+});
+
