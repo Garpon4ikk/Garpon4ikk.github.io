905249e01e1d58c1a2dd5dc62a77a7916cbf9d7a
Garpon4ikk <90471996+Garpon4ikk@users.noreply.github.com>
2021-11-26 11:59:35 +0300

    Add files via upload

--- myVizit/js/myScript.js
+++ myVizit/js/myScript.js
@@ -23,40 +23,69 @@ $(window).scroll(()=>{
 
 $('a[href^="#"]').click(function(){
   let valHref = $(this).attr("href");
-  $('html, body').animate ({scrollTop: $(valHref).offset().top - 70 + "px"});
+  $('html, body').animate ({scrollTop: $(valHref).offset().top - 100 + "px"});
 }); 
 
 
 
 
+/*onchange.OnChange = function () {
+  var type = document.getElementById('type').value
+  var design = $('input[name="design"]:checked').val();
+  var adapt = document.getElementById('adaptation').value
+  calcAll.onclick = function() {
+    /*РАСЧЁТ МАНЕЙ*/
+   /* var calc = 0;
 
+    /*расчёт типа*/
+   /* if (type == 1){
+      calc = calc + 2000;}
+    if (type == 2){
+      calc = calc + 1000;}
 
-var type = document.getElementById('type').value
-var design = $('input[name="design"]:checked').val();
-var adapt = document.getElementById('adaptation').value
-calcAll.onclick = function() {
-/*РАСЧЁТ МАНЕЙ*/
-var calc = 0;
+    /*расчёт дезигна*/
+   /* if (design == 2) {
+      calc = calc + 2000;} 
 
-/*расчёт типа*/
-if (type == 1){
-  calc = calc + 2000;}
-if (type == 2){
-  calc = calc + 1000;}
-console.log (type);
-/*расчёт дезигна*/
-if (design == 2) {
-  calc = calc + 2000;} 
-console.log (design);
-/*расчёт адаптации*/
-if (adapt ==1) {
-  calc = calc + 1000;;} 
-if (adaptation ==2) {
-  calc = calc + 1000;} 
-if (adaptation ==3) {
-  calc = calc + 2000;}
-  console.log (adapt);
-document.getElementById("calc").innerHTML = calc;
-console.log (calc);
-};
-console.log (calc);
+    /*расчёт адаптации*/
+   /* if (adapt ==1) {
+      calc = calc + 1000;;} 
+    if (adaptation ==2) {
+      calc = calc + 1000;} 
+    if (adaptation ==3) {
+      calc = calc + 2000;}
+
+    document.getElementById("calc").innerHTML = calc;
+  };
+};*/
+
+function OnFunction(val){
+  var type = document.getElementById('type').value
+  var design = $('input[name="design"]:checked').val();
+  var adapt = document.getElementById('adaptation').value
+  calcAll.onclick = function() {
+    /*РАСЧЁТ МАНЕЙ*/
+    var calc = 0;
+
+    /*расчёт типа*/
+    if (type == 1){
+      calc = calc + 2000;}
+    if (type == 2){
+      calc = calc + 1000;}
+
+    /*расчёт дезигна*/
+    if (design == 2) {
+      calc = calc + 2000;} 
+
+    /*расчёт адаптации*/
+    if (adapt ==1) {
+      calc = calc + 1000;;} 
+    if (adaptation ==2) {
+      calc = calc + 1000;} 
+    if (adaptation ==3) {
+      calc = calc + 2000;}
+
+    document.getElementById("calc").innerHTML = calc;
+  };
+}
+
